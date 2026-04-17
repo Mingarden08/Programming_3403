2ca47718e35ba6bdbc75fe6de8a9f9954e24d6bd
304-PC-06\Mirim <jeongone.min@gmail.com>
2026-04-17 14:47:22 +0900

    TodoListApp addTodo()

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -2,12 +2,28 @@ import './todolist.css';
 import TodoHeader from './components/TodoHeader.jsx';
 import TodoAdder from './components/TodoAdder.jsx';
 import TodoList from './components/TodoList.jsx';
+import { useState } from 'react';
+
+class Todo {
+    constructor(text) {
+        this.id = Date.now();       // PK
+        this.text = text;           // 내용
+        this.isCompleted = false;   // 완료 여부
+    }
+}
 
 function TodoListApp() {
+    const [todos, setTodos] = useState([]); // 할일 목록 저장 state
+    function addTodo(text) {
+        setTodos((todos) => [
+            ...todos,
+            new Todo(text)
+        ]);
+    }
     return (
         <div className="todo">
             <TodoHeader/>
-            <TodoAdder />
+            <TodoAdder addTodo={ addTodo } />
             <TodoList />
         </div>
     )
--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import Button from './Button';
 
-export default function TodoAdder() {
+export default function TodoAdder({ addTodo }) {
+    const [inputTodo, setInputTodo] = useState('');
+    function handleSubmit(event) {
+        event.preventDefault();
+        if(!inputTodo.trim()) return;
+        addTodo(inputTodo.trim());
+        setInputTodo('');
+    }
     return (
-        <form className="todo__form">
-            <input type="text" placeholder="할 일을 입력하세요" className="todo__input" />
+        <form className="todo__form" onSubmit={handleSubmit}>
+            <input 
+            type="text" 
+            placeholder="할 일을 입력하세요" 
+            className="todo__input" 
+            value={inputTodo}
+            onChange={(event) => setInputTodo(event.target.value)}/>
             <Button type="submit" className="todo__button todo__button--add">Add</Button>
         </form>
     );
